577b3328173b93d70501453ea432dac79a85a754
Alexey <alexledovit@gmail.com>
2022-10-21 01:46:51 +0300

    Links added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
           Weather App with React
         </h1>
         <Weather />
-    
+        <span>
+        This project was coded by <a href="https://github.com/Novemberman90">Olena Stratiuk</a>
+        and is <a href="https://github.com/Novemberman90/search_engien_weather_react"> open-sourced on GitHub</a>
+        and 
+        <a href="https://keen-pegasus-9f865e.netlify.app/">hosted om Netlify</a>
+        </span>
+        
     </div>
   );
 }
